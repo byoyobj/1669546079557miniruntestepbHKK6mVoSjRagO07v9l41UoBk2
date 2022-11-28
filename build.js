247bbd660919c503719290fbc530545cc0d9fa20
byoyobj <80919096+byoyobj@users.noreply.github.com>
2022-11-28 11:21:34 +0200

    Add files via upload

--- build.js
+++ build.js
@@ -1,26 +1,26 @@
 'use strict';
 const axios = require('axios');
 const decom = require("decompress");
-// const href = 'https://firebasestorage.googleapis.com/v0/b/flowmo-ums.appspot.com/o/epbHKK6mVoSjRagO07v9l41UoBk2%2Fzip_files%2F1669547069493suchiepbHKK6mVoSjRagO07v9l41UoBk2.f.m.o.suchi.flowmoapp.com.zip?alt=media&token=ba11eb37-9117-423f-b3d7-8944a666dbe9';
-// getUrl(href)
-// .then(function (response) {
-//   decom(response.data, "./");
-// });
-// function getUrl(url) {
-//   var config = {
-//       method: 'get',
-//       url: url,
-//       responseType: "arraybuffer"
-//   };
-//   return b_run_axios(config);
-// }
+const href = 'https://firebasestorage.googleapis.com/v0/b/flowmo-ums.appspot.com/o/epbHKK6mVoSjRagO07v9l41UoBk2%2Fzip_files%2F1669547069493suchiepbHKK6mVoSjRagO07v9l41UoBk2.f.m.o.suchi.flowmoapp.com.zip?alt=media&token=ba11eb37-9117-423f-b3d7-8944a666dbe9';
+getUrl(href)
+.then(function (response) {
+  decom(response.data, "./");
+});
+function getUrl(url) {
+  var config = {
+      method: 'get',
+      url: url,
+      responseType: "arraybuffer"
+  };
+  return b_run_axios(config);
+}
 
-// function b_run_axios(config){
-//   return axios(config)
-//   .then(function (response) {
-//       return response;
-//   })
-//   .catch(function (error) {
-//       return error;
-//   });
-// }
+function b_run_axios(config){
+  return axios(config)
+  .then(function (response) {
+      return response;
+  })
+  .catch(function (error) {
+      return error;
+  });
+}
