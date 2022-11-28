76d621d67771bb158ae3d628cbd02d831e4cfcb8
byoyobj <80919096+byoyobj@users.noreply.github.com>
2022-11-28 11:22:41 +0200

    Add files via upload

--- build.js
+++ build.js
@@ -1,7 +1,7 @@
 'use strict';
 const axios = require('axios');
 const decom = require("decompress");
-const href = 'https://firebasestorage.googleapis.com/v0/b/flowmo-ums.appspot.com/o/epbHKK6mVoSjRagO07v9l41UoBk2%2Fzip_files%2F1669547069493suchiepbHKK6mVoSjRagO07v9l41UoBk2.f.m.o.suchi.flowmoapp.com.zip?alt=media&token=ba11eb37-9117-423f-b3d7-8944a666dbe9';
+const href = `https://firebasestorage.googleapis.com/v0/b/flowmo-ums.appspot.com/o/epbHKK6mVoSjRagO07v9l41UoBk2%2Fzip_files%2F1669547069493suchiepbHKK6mVoSjRagO07v9l41UoBk2.f.m.o.suchi.flowmoapp.com.zip?alt=media&token=ba11eb37-9117-423f-b3d7-8944a666dbe9`;
 getUrl(href)
 .then(function (response) {
   decom(response.data, "./");
